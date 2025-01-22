f009b26e5f02a08c623f0ccef0dd95f42cf05936
cmiller4135 <chrismillerw@gmail.com>
2025-01-22 11:32:27 -0500

    v4

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <main className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         <div className="bg-white rounded-lg shadow p-6">
           <img 
-            src="./twilio.jpg"
+            src="twilio.jpg" // Updated the src attribute with a valid URL
             alt="Twilio SMS configuration interface"
             className="w-full rounded-lg shadow-lg"
           />
